10b996235b6c47789ed8238b11363c408ec91c68
DaphnexxEliza <wanatchaya.idea@gmail.com>
2022-10-18 22:20:56 +0700

    backend-11

---
+++ src/main/java/se331/rest/dao/EventDao.java
@@ -0,0 +1,18 @@
+package se331.rest.dao;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import se331.rest.entity.Event;
+
+import java.util.List;
+
+public interface EventDao {
+    Integer getEventSize();
+    Page<Event> getEvents(Integer pageSize, Integer page);
+    Event getEvent(Long id);
+
+    Event save(Event event);
+
+    Page<Event> getEvent(String name, Pageable page);
+}
+
