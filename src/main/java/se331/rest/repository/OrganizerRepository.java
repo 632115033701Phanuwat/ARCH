371a4b996a0104a927f7cde22990367a96de93dd
DaphnexxEliza <wanatchaya.idea@gmail.com>
2022-10-19 18:13:53 +0700

    Bid

--- src/main/java/se331/rest/repository/OrganizerRepository.java
+++
@@ -1,8 +0,0 @@
-package se331.rest.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import se331.rest.entity.Organizer;
-
-public interface OrganizerRepository extends JpaRepository<Organizer,Long> {
-}
-
